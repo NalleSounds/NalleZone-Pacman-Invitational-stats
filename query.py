1accaab48b2111c9d600e92c04c18e4af14ea973
NalleSounds <melomelitori@gmail.com>
2023-03-24 08:12:54 +0100

    changed formatting and renamed and added query.py (not finished)

--- query.py
+++ query.py
@@ -1,7 +1,9 @@
+# GUI to prompt data from file
+
 import json
 
 with open('matches.json') as matches_file:
-  file_contents = matches_file.read()
+    file_contents = matches_file.read()
 
 print(file_contents)
 
